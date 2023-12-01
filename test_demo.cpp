3a00eea3545a54f9c523ce8f9e4617fc0312b108
will <2623556566@qq.com>
2023-12-01 15:42:57 +0800

    asd

--- test_demo.cpp
+++ test_demo.cpp
@@ -1,13 +1,18 @@
-#include <ros/ros.h>
 #include <string>
-
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/point_types.h>
-#include <eigen3/Eigen/Dense>
-#include <pcl/filters/extract_indices.h>
-
 #include <sstream>
 #include <iomanip>
+
+#include <Eigen/Dense>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
+
 #include "LinK3D_Extractor.h"
 #include "BoW3D.h"
 
@@ -15,7 +20,7 @@ using namespace std;
 using namespace BoW3D;
 
 // Parameters of LinK3D
-int nScans = 64; // Number of LiDAR scan lines
+int nScans = 32; // Number of LiDAR scan lines
 float scanPeriod = 0.1;
 float minimumRange = 0.1;
 float distanceTh = 0.4;
@@ -26,24 +31,140 @@ float thr = 3.5;
 int thf = 5;
 int num_add_retrieve_features = 5;
 
-// Here, the KITTI's point cloud with '.bin' format is read.
-vector<float> read_lidar_data(const std::string lidar_data_path)
+// #define VEL_TIMESTAMP_TYPE float
+#define VEL_TIMESTAMP_TYPE double
+// #define VEL_TIMESTAMP_FIELD time
+#define VEL_TIMESTAMP_FIELD timestamp
+
+namespace velodyne_ros
 {
-    std::ifstream lidar_data_file;
-    lidar_data_file.open(lidar_data_path, std::ifstream::in | std::ifstream::binary);
-    if (!lidar_data_file)
+  struct EIGEN_ALIGN16 Point
+  {
+    PCL_ADD_POINT4D;
+    float intensity;
+    VEL_TIMESTAMP_TYPE VEL_TIMESTAMP_FIELD;
+    uint16_t ring;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  };
+} // namespace velodyne_ros
+POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_ros::Point,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    (VEL_TIMESTAMP_TYPE, VEL_TIMESTAMP_FIELD, VEL_TIMESTAMP_FIELD)
+    (std::uint16_t, ring, ring)
+)
+
+void pcl_convert(pcl::PointCloud<velodyne_ros::Point> &pl_orig_velo, pcl::PointCloud<PointType> &current_cloud)
+{
+    int plsize = pl_orig_velo.points.size();
+    for (int i = 0; i < plsize; i++)
     {
-        cout << "Read End..." << endl;
-        exit(-1);
+        PointType added_pt;
+        added_pt.x = pl_orig_velo.points[i].x;
+        added_pt.y = pl_orig_velo.points[i].y;
+        added_pt.z = pl_orig_velo.points[i].z;
+        added_pt.intensity = pl_orig_velo.points[i].intensity;
+        current_cloud.points.push_back(added_pt);
+    }
+}
+
+void read_rosbag(const std::string &bagfile, const std::vector<std::string> &topics,
+                 std::shared_ptr<BoW3D::LinK3D_Extractor> &linK3dExtractor,
+                 std::shared_ptr<BoW3D::BoW3D> &BoW3D)
+{
+    rosbag::Bag bag;
+
+    try
+    {
+        bag.open(bagfile, rosbag::bagmode::Read);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << bagfile << '\n';
+        std::cerr << e.what() << '\n';
     }
 
-    lidar_data_file.seekg(0, std::ios::end);
-    const size_t num_elements = lidar_data_file.tellg() / sizeof(float);
-    lidar_data_file.seekg(0, std::ios::beg);
+    rosbag::View view(bag, rosbag::TopicQuery(topics));
 
-    std::vector<float> lidar_data_buffer(num_elements);
-    lidar_data_file.read(reinterpret_cast<char *>(&lidar_data_buffer[0]), num_elements * sizeof(float));
-    return lidar_data_buffer;
+    ROS_INFO("Test rosbag %s...", bagfile.c_str());
+
+    int cnt = 0;
+    int load_keyframe = 0;
+
+    for (const rosbag::MessageInstance& msg : view)
+    {
+        if (!msg.isType<sensor_msgs::PointCloud2>())
+            continue;
+
+        cnt++;
+
+        if (cnt % 100 == 0)
+            continue;
+
+        load_keyframe++;
+
+        sensor_msgs::PointCloud2::ConstPtr cloud = msg.instantiate<sensor_msgs::PointCloud2>();
+        pcl::PointCloud<velodyne_ros::Point> pl_orig_velo;
+        pcl::PointCloud<PointType>::Ptr current_cloud(new pcl::PointCloud<PointType>());
+        pcl::fromROSMsg(*cloud, pl_orig_velo);
+        pcl_convert(pl_orig_velo, *current_cloud);
+        std::shared_ptr<Frame> pCurrentFrame = std::make_shared<Frame>(linK3dExtractor, current_cloud);
+        BoW3D->update(pCurrentFrame);
+    }
+    ROS_INFO("Read lidar keyframe %d.", load_keyframe);
+
+    bag.close();
+}
+
+void test_rosbag(const std::string &bagfile, const std::vector<std::string> &topics,
+                 std::shared_ptr<BoW3D::LinK3D_Extractor> &linK3dExtractor,
+                 std::shared_ptr<BoW3D::BoW3D> &BoW3D)
+{
+    rosbag::Bag bag;
+    ROS_WARN("test rosbag start.");
+
+    try
+    {
+        bag.open(bagfile, rosbag::bagmode::Read);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << bagfile << '\n';
+        std::cerr << e.what() << '\n';
+    }
+
+    rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+    ROS_INFO("Test rosbag %s...", bagfile.c_str());
+
+    int load_keyframe = 0;
+
+    for (const rosbag::MessageInstance& msg : view)
+    {
+        if (!msg.isType<sensor_msgs::PointCloud2>())
+            continue;
+
+        load_keyframe++;
+
+        sensor_msgs::PointCloud2::ConstPtr cloud = msg.instantiate<sensor_msgs::PointCloud2>();
+        pcl::PointCloud<velodyne_ros::Point> pl_orig_velo;
+        pcl::PointCloud<PointType>::Ptr current_cloud(new pcl::PointCloud<PointType>());
+        pcl::fromROSMsg(*cloud, pl_orig_velo);
+        pcl_convert(pl_orig_velo, *current_cloud);
+        std::shared_ptr<Frame> pCurrentFrame = std::make_shared<Frame>(linK3dExtractor, current_cloud);
+
+        int loopFrameId = -1;
+        Eigen::Matrix3d loopRelR;
+        Eigen::Vector3d loopRelt;
+        BoW3D->retrieve(pCurrentFrame, loopFrameId, loopRelR, loopRelt);
+        if (loopFrameId != -1)
+            ROS_WARN("%d", loopFrameId);
+    }
+    ROS_WARN("test rosbag end! num = %d", load_keyframe);
+
+    bag.close();
 }
 
 int main(int argc, char **argv)
@@ -51,81 +172,14 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "BoW3D");
     ros::NodeHandle nh;
 
-    /*Please replace the dataset folder path with the path in your computer. KITTI's 00, 02, 05, 06, 07, 08 have loops*/
-    string dataset_folder;
-    dataset_folder = "/home/cuiyunge/dataset/velodyne/"; // The last '/' should be added
+    string bag_path = "/home/will/data/work_data/shanghai_base4.bag";
+    std::vector<std::string> topics = {"/drivers/top_lidar_origin"};
 
     std::shared_ptr<BoW3D::LinK3D_Extractor> pLinK3dExtractor = std::make_shared<BoW3D::LinK3D_Extractor>(nScans, scanPeriod, minimumRange, distanceTh, matchTh);
     std::shared_ptr<BoW3D::BoW3D> pBoW3D = std::make_shared<BoW3D::BoW3D>(pLinK3dExtractor, thr, thf, num_add_retrieve_features);
 
-    size_t cloudInd = 0;
-
-    ros::Rate LiDAR_rate(10); // LiDAR frequency 10Hz
-    while (ros::ok())
-    {
-        std::stringstream lidar_data_path;
-        lidar_data_path << dataset_folder << std::setfill('0') << std::setw(6) << cloudInd << ".bin";
-        vector<float> lidar_data = read_lidar_data(lidar_data_path.str());
-
-        pcl::PointCloud<PointType>::Ptr current_cloud(new pcl::PointCloud<PointType>());
-        for (std::size_t i = 0; i < lidar_data.size(); i += 4)
-        {
-            PointType point;
-            point.x = lidar_data[i];
-            point.y = lidar_data[i + 1];
-            point.z = lidar_data[i + 2];
-
-            current_cloud->push_back(point);
-        }
-
-        std::shared_ptr<Frame> pCurrentFrame = std::make_shared<Frame>(pLinK3dExtractor, current_cloud);
-
-        if (pCurrentFrame->mnId < 2)
-        {
-            pBoW3D->update(pCurrentFrame);
-        }
-        else
-        {
-            int loopFrameId = -1;
-            Eigen::Matrix3d loopRelR;
-            Eigen::Vector3d loopRelt;
-
-            clock_t start, end;
-            double time;
-            start = clock();
-
-            pBoW3D->retrieve(pCurrentFrame, loopFrameId, loopRelR, loopRelt);
-
-            end = clock();
-            time = ((double)(end - start)) / CLOCKS_PER_SEC;
-
-            pBoW3D->update(pCurrentFrame);
-
-            if (loopFrameId == -1)
-            {
-                cout << "-------------------------" << endl;
-                cout << "Detection Time: " << time << "s" << endl;
-                cout << "Frame" << pCurrentFrame->mnId << " Has No Loop..." << endl;
-            }
-            else
-            {
-                cout << "--------------------------------------" << endl;
-                cout << "Detection Time: " << time << "s" << endl;
-                cout << "Frame" << pCurrentFrame->mnId << " Has Loop Frame" << loopFrameId << endl;
-
-                cout << "Loop Relative R: " << endl;
-                cout << loopRelR << endl;
-
-                cout << "Loop Relative t: " << endl;
-                cout << "   " << loopRelt.x() << " " << loopRelt.y() << " " << loopRelt.z() << endl;
-            }
-        }
-
-        cloudInd++;
-
-        ros::spinOnce();
-        LiDAR_rate.sleep();
-    }
+    read_rosbag(bag_path, topics, pLinK3dExtractor, pBoW3D);
+    test_rosbag(bag_path, topics, pLinK3dExtractor, pBoW3D);
 
     return 0;
 }
